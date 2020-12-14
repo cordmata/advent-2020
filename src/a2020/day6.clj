3208a15406246f99ebaea5158d9ccddba4e50ecf
Matt Cordial <Matt.Cordial@experian.com>
2020-12-13 19:40:07 -0700

    add day6 part2

--- src/a2020/day6.clj
+++ src/a2020/day6.clj
@@ -5,26 +5,30 @@
     [clojure.java.io :as io]
     [clojure.set :as set]))
 
-(def group-example  ["abcx" "abcy" "abcz"])
-(def example-input "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb\n")
-
-(defn find-yes-answers [group-answers] (reduce set/union (map set group-answers)))
-
 (defn parse-input [inpt]
   (for [group (str/split inpt #"\n\n")] (str/split-lines group)))
 
 (defn part1 [inpt]
-  (->> inpt
-       parse-input
-       (map find-yes-answers)
+  (->> (parse-input inpt)
+       (map #(reduce set/union (map set %)))
        (map count)
        (reduce +)))
 
-(deftest test-count-yeses
-  (is (= 6 (count (find-yes-answers group-example)))))
+(defn part2 [inpt]
+  (->> (parse-input inpt)
+       (map #(reduce set/intersection (map set %)))
+       (map count)
+       (reduce +)))
 
-(deftest test-part1-example
+(def example-input "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb\n")
+(def actual-input (slurp (io/resource "customs_forms.txt")))
+
+(deftest test-part1
   (is (= 11 (part1 example-input)))
-  (is (= 6551 (part1 (slurp (io/resource "customs_forms.txt"))))))
+  (is (= 6551 (part1 actual-input))))
+
+(deftest test-part2
+  (is (= 6 (part2 example-input)))
+  (is (= 3358 (part2 actual-input))))
 
 (run-tests)
