0ab3504b8192d72cc649afb204347138509d0c12
Matt Cordial <Matt.Cordial@experian.com>
2020-12-16 20:50:09 -0700

    add references to new modules in core

--- src/a2020/core.clj
+++ src/a2020/core.clj
@@ -1,9 +1,12 @@
 (ns a2020.core
   (:require
-    a2020.day1
-    a2020.day2
-    a2020.day3
-    a2020.day4)
+   a2020.day1
+   a2020.day2
+   a2020.day3
+   a2020.day4
+   a2020.day5
+   a2020.day6
+   a2020.day7)
   (:gen-class))
 
 (defn -main []
